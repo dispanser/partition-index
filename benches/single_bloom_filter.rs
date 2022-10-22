90f4a18814015ca429f71746ef71166592c9ff95
Thomas Peiselt <pi@kulturguerilla.org>
2022-10-22 07:38:43 +0200

    Add another criterion test for inserting with varying number of hash functions

--- benches/single_bloom_filter.rs
+++ benches/single_bloom_filter.rs
@@ -1,7 +1,7 @@
 extern crate bloom_lake;
 
 use bloom_lake::bloom::{Filter, PaperBloom};
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
 fn insert_n(n: u64, d: u64, m: u64) -> PaperBloom {
     let mut filter = PaperBloom::new(d, m);
@@ -13,7 +13,7 @@ fn contains(f: &dyn Filter) -> bool {
     f.contains(0)
 }
 
-fn insert_bench(c: &mut Criterion) {
+fn insert_bench_vary_n(c: &mut Criterion) {
     let mut group = c.benchmark_group("insert_varying size");
     for n in [10_000, 100_000, 1_000_000] {
         group.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, &n| {
@@ -22,16 +22,30 @@ fn insert_bench(c: &mut Criterion) {
     }
 }
 
+fn insert_bench_vary_d(c: &mut Criterion) {
+    let mut group = c.benchmark_group("insert_varying_d");
+    for d in [2, 4, 8, 16] {
+        group.bench_with_input(BenchmarkId::from_parameter(d), &d, |b, &d| {
+            b.iter(|| insert_n(100_000, d, 1_400_000))
+        });
+    }
+}
+
 fn contains_bench_small(c: &mut Criterion) {
     let mut group = c.benchmark_group("contains_small_set");
-    for d in 2..=10 {
+    for d in [2, 4, 8, 16] {
         // precompute filter outside of the contains benchmark
         let filter = insert_n(10_000, 10, 1 << 16);
-        group.bench_with_input(BenchmarkId::from_parameter(d), &d, |b, &d| {
+        group.bench_with_input(BenchmarkId::from_parameter(d), &d, |b, &_| {
             b.iter(|| contains(black_box(&filter)))
         });
     }
 }
 
-criterion_group!(benches, contains_bench_small, insert_bench);
+criterion_group!(
+    benches,
+    contains_bench_small,
+    insert_bench_vary_d,
+    insert_bench_vary_n
+);
 criterion_main!(benches);
