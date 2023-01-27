ba7dd8f2b7b026540f149053e748d4595eda5860
Thomas Peiselt <pi@kulturguerilla.org>
2023-01-27 19:57:27 +0100

    Add basic tool to estimate fp / fn fp_rate
    - fn is zero, so expected partitions are found
    - fp rate is 0.1%, expected would be 0.01%
      - 10k partitions
      - 10k elements per partition
      - 4k buckets -> 3 elements per bucket / partition, 60k bucket file size
      - 6 / 2^16 expected fp rate

---
+++ src/bin/benchmark_query_correctness.rs
@@ -0,0 +1,52 @@
+use partition_index::{
+    self, benchmarks::BenchmarkPartition, index::poc::PersistentIndex, index::PartitionFilter,
+};
+use rand::{distributions::Uniform, Rng, SeedableRng};
+use std::time::SystemTime;
+
+fn main() -> anyhow::Result<()> {
+    use std::env;
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let num_queries: u64 = args[2].parse()?;
+
+    let mut index =
+        PersistentIndex::<BenchmarkPartition>::try_load_from_disk(file_path.to_string())?;
+    let p0 = index.partitions().next().expect("invalid: empty index");
+    let max_value = p0.elements() * index.num_partitions() as u64;
+    let start_querying = SystemTime::now();
+
+    let mut false_positives = 0u64;
+    let mut false_negatives = 0u64;
+    let value_distribution = Uniform::new(0, max_value);
+    let mut data_rng = rand_xoshiro::Xoshiro256PlusPlus::seed_from_u64(1337);
+    for _i in 0..num_queries {
+        let value = data_rng.sample(value_distribution);
+        let query_result = index.query(value)?;
+        if query_result
+            .iter()
+            .any(|p| value >= p.start && value < p.start + p.length)
+        {
+            false_positives += query_result.len() as u64 - 1;
+        } else {
+            false_positives += query_result.len() as u64;
+            false_negatives += 1;
+        }
+        // eprintln!("tp;matches {} -> {:#?}", value, query_result);
+    }
+    index.persist()?;
+    let query_duration = start_querying.elapsed()?;
+    let fp_rate =
+        false_positives as f64 / (num_queries as u128 * index.num_partitions() as u128) as f64;
+    eprintln!(
+        "tp;correctness: {} false positives, {} false negatives, fp-rate {}",
+        false_positives, false_negatives, fp_rate,
+    );
+    eprintln!(
+        "tp;bench query: queried {} elems in {:?} ({:?} ops)",
+        num_queries,
+        query_duration,
+        num_queries as u128 * 1000 / query_duration.as_millis()
+    );
+    Ok(())
+}
