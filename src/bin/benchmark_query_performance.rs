a0645cc0d86bc392c46b44e869d3e8620f1db2a2
Thomas Peiselt <pi@kulturguerilla.org>
2023-01-30 13:52:49 +0100

    Remove redundant call to persist in query performance benchmark
    oopsie

--- src/bin/benchmark_query_performance.rs
+++ src/bin/benchmark_query_performance.rs
@@ -12,12 +12,11 @@ fn main() -> anyhow::Result<()> {
     let file_path = &args[1];
     let num_queries: u64 = args[2].parse()?;
 
-    let mut index = PersistentIndex::<BenchmarkPartition>::try_load_from_disk(file_path.to_string())?;
+    let index = PersistentIndex::<BenchmarkPartition>::try_load_from_disk(file_path.to_string())?;
     let start_querying = SystemTime::now();
     for i in 0..num_queries {
         index.query(i)?;
     }
-    index.persist()?;
     let query_duration = start_querying.elapsed()?;
     eprintln!(
         "tp;bench query: queried {} elems in {:?} ({:?} ops)",
