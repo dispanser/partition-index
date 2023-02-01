d72d1e0dfb195a60572618ade5abc7b405e35eb1
Thomas Peiselt <pi@kulturguerilla.org>
2023-02-01 16:49:46 +0100

    Use rayon to run queries concurrently
    This gives really interesting performance where on a "small" data set,
    we scale by a factor of 6x (linear scaling with 8 cores + reduced clock speed
    per core due to CPU power envelope), whereas for a "large" data set, the
    actual speedup from concurrently running the queries is 2x.
    
    It seems that we spend way more time in "system" (50% instead of 25%),
    so it's probably the I/O that is producing most of the extra work.

--- src/bin/benchmark_query_performance.rs
+++ src/bin/benchmark_query_performance.rs
@@ -4,7 +4,15 @@ use partition_index::{
     index::poc::PersistentIndex,
     index::PartitionFilter,
 };
-use std::time::SystemTime;
+use rstats::{Stats, Median, noop};
+use std::time::{SystemTime, Duration};
+use rayon::prelude::*;
+
+fn run_query(index: &PersistentIndex<BenchmarkPartition>, i: u64) -> anyhow::Result<Duration>{
+    let s = SystemTime::now();
+    index.query(i)?;
+    Ok(s.elapsed()?)
+}
 
 fn main() -> anyhow::Result<()> {
     use std::env;
@@ -13,10 +21,12 @@ fn main() -> anyhow::Result<()> {
     let num_queries: u64 = args[2].parse()?;
 
     let index = PersistentIndex::<BenchmarkPartition>::try_load_from_disk(file_path.to_string())?;
+    let queries = Vec::from_iter(0..num_queries);
+    // let queries = 0..num_queries;
     let start_querying = SystemTime::now();
-    for i in 0..num_queries {
-        index.query(i)?;
-    }
+    let results: Vec<f64> = queries.par_iter().map(|i| { 
+        run_query(&index, *i).unwrap().as_micros() as f64
+    }).collect();
     let query_duration = start_querying.elapsed()?;
     eprintln!(
         "tp;bench query: queried {} elems in {:?} ({:?} ops)",
@@ -24,5 +34,8 @@ fn main() -> anyhow::Result<()> {
         query_duration,
         num_queries as u128 * 1000 / query_duration.as_millis()
     );
+    eprintln!("Median     {}", results.medstats(&mut noop)?);
+    eprintln!("Arithmetic {}", results.ameanstd()?);
+    eprintln!("{}", results.medinfo(&mut noop)?);
     Ok(())
 }
